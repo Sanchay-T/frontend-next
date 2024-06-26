c9e09b1d07b27da6d15316306b54f9b00b7ded9f
Sanchay Thalnerkar <thalnerkarsanchay17@gmail.com>
2024-06-26 14:19:18 +0530

    done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,7 +7,6 @@ export const metadata = {
   title: "Blog",
   description: "My thoughts on software development, life, and more.",
 };
-
 export default async function BlogPage() {
   const posts = await getBlogPosts();
 
